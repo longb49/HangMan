3654e2a7593cb8121a5d82c8be5e5f72284a873f
Ben Long <113473264+longb49@users.noreply.github.com>
2022-10-14 22:16:20 -0600

    Well, it works

--- Library.cs
+++ Library.cs
@@ -1,8 +1,18 @@
 class Library
 {
     private List<string> library = new List<string>
-    (
-        "reject",
-        ""
-    );
+    {
+        "Apple",
+        "Cat",
+        "Dragon",
+        "Hero",
+        "King"
+    };
+
+    public string randomWord(List<string> library)
+    {
+        var rand = new Random();
+        string word = library[rand.Next(library.Count)];
+        return word;
+    }
 }
