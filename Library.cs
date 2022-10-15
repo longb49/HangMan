2fdace15bbb5ebcc1f6fd857800eb64b56678e61
Ben Long <113473264+longb49@users.noreply.github.com>
2022-10-14 22:30:26 -0600

    Added a difficulty setting

--- Library.cs
+++ Library.cs
@@ -2,17 +2,73 @@ class Library
 {
     private List<string> library = new List<string>
     {
+        "Actor",
+        "Advertisement",
+        "Afternoon",
+        "Airport",
+        "Ambulance",
+        "Animal",
+        "Answer",
         "Apple",
-        "Cat",
-        "Dragon",
-        "Hero",
-        "King"
+        "Army",
+        "Australia",
     };
 
-    public string randomWord(List<string> library)
+    private string randomWord(List<string> library)
     {
         var rand = new Random();
         string word = library[rand.Next(library.Count)];
+        return word;
+    }
+
+    public string getWord(string difficulty) // Difficulty requires a string of either "easy", "medium", or "hard"
+    {
+        string word = "";
+        if (difficulty == "easy")
+        {
+            while (word == "")
+            {
+                string word0 = randomWord(library);
+                if (word0.Length < 5)
+                {
+                    word = word0;
+                }
+                else
+                {
+                    word = "";
+                }
+            }
+        }
+        else if (difficulty == "medium")
+        {
+            while (word == "")
+            {
+                string word0 = randomWord(library);
+                if (5 <= word0.Length && word0.Length < 7)
+                {
+                    word = word0;
+                }
+                else
+                {
+                    word = "";
+                }
+            }
+        }
+        else if (difficulty == "hard")
+        {
+            while (word == "")
+            {
+                string word0 = randomWord(library);
+                if (7 <= word0.Length)
+                {
+                    word = word0;
+                }
+                else
+                {
+                    word = "";
+                }
+            }
+        }
         return word;
     }
 }
