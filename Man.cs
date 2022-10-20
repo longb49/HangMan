d125df45e72779bb3896924b5cc9ca1c6cbb7517
Ben Long <113473264+longb49@users.noreply.github.com>
2022-10-20 13:07:02 -0600

    Created the man

--- Man.cs
+++ Man.cs
@@ -1,5 +1,29 @@
 class Man
 {
-    int health = 5; // You have 5 guesses
-    
+    private List<string> player_states = new List<string>();
+    private int health = 4;
+
+    private Man()
+    {
+        string line0 = @"^^^^^^^^^";
+        string line1 = @"   ___   ";
+        string line2 = @"  /___\  ";
+        string line3 = @"  \   /  ";
+        string line4 = @"   \ /   ";
+        string head =  @"    o    ";
+        string body =  @"   /|\   ";
+        string legs =  @"   / \   ";
+        string dead =  @"    x    ";
+
+        player_states.Add($"{dead}\n{body}\n{legs}\n{line0}");
+        player_states.Add($"{line4}\n{head}\n{body}\n{legs}\n{line0}");
+        player_states.Add($"{line3}\n{line4}\n{head}\n{body}\n{legs}\n{line0}");
+        player_states.Add($"{line2}\n{line3}\n{line4}\n{head}\n{body}\n{legs}\n{line0}");
+        player_states.Add($"{line1}\n{line2}\n{line3}\n{line4}\n{head}\n{body}\n{legs}\n{line0}");
+    }
+
+    public string getMan(List<string> player_states, int health)
+    {
+        return player_states[health];
+    }
 }
